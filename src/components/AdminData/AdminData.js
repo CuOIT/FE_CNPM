17a2f0be84ecfd5b149e44da9f8e4ec1340a0d21
sucrub <bachspham@gmail.com>
2023-02-18 22:23:06 +0700

    fix some sizing issue like idk what the f is goin on

--- src/components/AdminData/AdminData.js
+++ src/components/AdminData/AdminData.js
@@ -82,7 +82,7 @@ const AdminDataPage = () => {
                         <img className="userava" src="https://images.are.na/eyJidWNrZXQiOiJhcmVuYV9pbWFnZXMiLCJrZXkiOiI4MDQwOTc0L29yaWdpbmFsX2ZmNGYxZjQzZDdiNzJjYzMxZDJlYjViMDgyN2ZmMWFjLnBuZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTIwMCwiaGVpZ2h0IjoxMjAwLCJmaXQiOiJpbnNpZGUiLCJ3aXRob3V0RW5sYXJnZW1lbnQiOnRydWV9LCJ3ZWJwIjp7InF1YWxpdHkiOjkwfSwianBlZyI6eyJxdWFsaXR5Ijo5MH0sInJvdGF0ZSI6bnVsbH19?bc=0" alt="Ava User"></img>
                         <h2 className="justname">{dataUserRedux.user_name}</h2>
                     </div>
-                    <div className="option">
+                    <div className="optionofhistory">
                         <div>
                             <button onClick={() => setShowCheck1(true)} className="thongkeelement main">Doanh thu cửa hàng</button>
                             <Form
